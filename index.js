69f69352a611d62399e8e149ef10f75536fd3e16
Endo1905 <lbujocz@gmail.com>
2023-01-31 19:28:10 +0100

    section one to finish

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ function typing() {
     if (i < hello.length) {
         document.getElementById("hello").textContent += hello.charAt(i);
         i++;
-        setTimeout(typing, 200);
+        setTimeout(typing, 100);
     }
 }
-
-typing();
 
 var date = new Date();
 var hours = date.getHours();
@@ -23,9 +21,28 @@ function time() {
         text.textContent = "Miłego popołudnia!"
     }
     else {
-        text.textContent = "...I przede wszystkim zasłużonego wypoczunku na wieczór! :D"
+        text.textContent = "...I przede wszystkim zasłużonego wypoczunku na wieczór! :)"
     }
 }
 time();
 typing();
 console.log(hours)
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
