6305040cf4b99ac38906891f241901eaba595609
Endo1905 <Endo1905@lbujocz@gmail.com>
2023-02-02 16:52:04 +0000

    Finalizing header

--- index.js
+++ index.js
@@ -46,3 +46,22 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+const observer = new IntersectionObserver((entries) => 
+{
+    entries.forEach((entry) => 
+    {
+        console.log(entry)
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('show');
+        }
+        // else 
+        // {
+        //     entry.target.classList.remove('show');
+        // }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
